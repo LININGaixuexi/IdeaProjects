b55433e314324131040d085d39983c44973c8828
Lining <Lining@qq.com>
2022-12-23 13:45:14 +0800

    push 测试

--- javaSE2/gittest1/src/Test01.java
+++ javaSE2/gittest1/src/Test01.java
@@ -4,5 +4,7 @@ public class Test01 {
         System.out.println("测试");
         System.out.println("测试");
         System.out.println("test 测试");
+        System.out.println("push 测试");
     }
-}
+    }
+
