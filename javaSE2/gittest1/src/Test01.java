2aed09160970631bffececc0c53c9e91d003169d
LININGaixuexi <119659220+LININGaixuexi@users.noreply.github.com>
2022-12-23 13:46:20 +0800

    pull test
    在座都是垃圾

--- javaSE2/gittest1/src/Test01.java
+++ javaSE2/gittest1/src/Test01.java
@@ -5,6 +5,7 @@ public class Test01 {
         System.out.println("测试");
         System.out.println("test 测试");
         System.out.println("push 测试");
+        System.out.println("pull 测试");
     }
     }
 
